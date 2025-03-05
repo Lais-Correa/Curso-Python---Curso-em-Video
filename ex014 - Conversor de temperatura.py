eefacce546dbdfdcb352d29bf131e2d82be35f9e
Lais Correa <lais.crr@gmail.com>
2025-03-05 12:31:21 +0000

    Update ex014 - Conversor de temperatura.py

--- ex014 - Conversor de temperatura.py
+++ ex014 - Conversor de temperatura.py
@@ -1,3 +1,5 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
 c = float(input('Informe a temperatura ªC: '))
 f = ((9 * c) / 5) + 32
 print('A temperatura de {}ªC, corresponde a {}ªF'. format(c,f))
