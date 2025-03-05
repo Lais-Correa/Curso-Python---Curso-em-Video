90e51a449b8b37a8b77d8dbc7f1a40685a310db1
Lais Correa <lais.crr@gmail.com>
2025-03-05 12:53:06 +0000

    Create ex015 - Aluguel de carros.py

---
+++ ex015 - Aluguel de carros.py
@@ -0,0 +1,18 @@
+#Escreva um programa que pergunte a quantidade de Km percorridos
+# por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
+# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+print('\nSistema de aluguel de carros. \nInforme as informações necessarias para os cálculos:')
+print('-----------------------------------------------------')
+km = float(input('Informe os Km precorridos: '))
+dias = int(input('Por quantos dias o carro foi alugado: '))
+
+rs_km = (km * 0.15)
+rs_dias = dias * 60
+total = rs_km + rs_dias
+
+print('\nO total a pagar é de: {:.2f}'. format(total))
+print('-----------------------------------------------------')
+print('Segue abaixo a descrição dos valores: ')
+print('KM - ',  km, 'X R$ 0,15 por KM rodado = R${:.2f}' .format(rs_km))
+print('DIAS - ',  dias, 'X R$60,00 por dias alugados = R${:.2f}' .format(rs_dias))
