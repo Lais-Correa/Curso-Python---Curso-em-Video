c4beacb33d3ec2b7749920bfc32a81d63160f488
Lais Correa <lais.crr@gmail.com>
2025-03-07 10:20:13 +0000

    Update ex018 - Seno Cosseno e Tangente.py

--- ex018 - Seno Cosseno e Tangente.py
+++ ex018 - Seno Cosseno e Tangente.py
@@ -1,4 +1,6 @@
-import math
+#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+mport math
 
 angulo = float(input('Digite o ângulo que você deseja: '))
 seno = math.sin(math.radians(angulo))
