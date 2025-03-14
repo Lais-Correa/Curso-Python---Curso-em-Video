140d97cde8139bae58654d2c737b2396ee4de162
Lais Correa <lais.crr@gmail.com>
2025-03-14 11:44:03 +0000

    Update ex039 - Alistamento militar.py

--- ex039 - Alistamento militar.py
+++ ex039 - Alistamento militar.py
@@ -5,15 +5,32 @@
 from datetime import date
 
 nasc = int(input('Digite o ano de nascimento: '))
+sexo = input('Digite seu sexo: (Opções aceitas: MASCULINO ou FEMININO) ') .strip().upper()
+
 ano_atual = date.today().year
 idade = ano_atual - nasc
+
 print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, ano_atual))
 
-if idade < 18:
-    print('Ainda faltam {} anos para o alistamento'.format(18-idade))
-    print('Você deve se alistar em {}'.format(nasc+18))
-elif idade == 18:
-    print('Você deve se alistar IMEDIATAMENTE!')
-elif idade > 18:
-    print('Você ja deveria ter se alistado há {} anos'.format(idade-18))
-    print('Seu alistamento foi em {}'.format(nasc+18))
+if sexo == 'MASCULINO':
+    if idade < 18:
+        print('Ainda faltam {} anos para o alistamento'.format(18-idade))
+        print('Você deve se alistar em {}'.format(nasc+18))
+    elif idade == 18:
+        print('Você deve se alistar IMEDIATAMENTE!')
+    elif idade > 18:
+        print('Você ja deveria ter se alistado há {} anos'.format(idade-18))
+        print('Seu alistamento foi em {}'.format(nasc+18))
+
+elif sexo == 'FEMININO':
+    print('O alistamento militar FEMININO não é obrigatório.')
+    opcao = input('Você deseja se alistar voluntáriamente? (S/N)').strip().upper()
+
+    if opcao == 'S' and 18 <= idade <= 48:
+        print('Você pode ir à junta militar mais próxima para se alistar voluntariamente')
+    else:
+        print('Você não precisa se alistar.')
+
+else:
+    print('Opção inválida')
+    
