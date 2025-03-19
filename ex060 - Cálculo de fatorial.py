3014f7ef2dfaf1dd09b2c773b9377fd4e765fdf7
Lais Correa <lais.crr@gmail.com>
2025-03-19 11:17:25 +0000

    Create ex060 - Cálculo de fatorial.py

---
+++ ex060 - Cálculo de fatorial.py
@@ -0,0 +1,19 @@
+#Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+n = int(input('Digite um número para calcular o seu fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n),end='')
+while c > 0:
+    print('{}'.format(c),end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+# usando modulos do Python:
+'''from math import factorial
+
+n = int(input('Digite um número para calcular o seu fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}'.format(n,f))'''
