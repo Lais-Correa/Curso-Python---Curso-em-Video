f145db1ff86d0e311e7f517fccbe0efe794ce95a
Lais Correa <lais.crr@gmail.com>
2025-03-27 11:59:36 +0000

    Update 075 - Análise de dados em uma tupla.py

--- 075 - Análise de dados em uma tupla.py
+++ 075 - Análise de dados em uma tupla.py
@@ -4,16 +4,20 @@ B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares.'''
 
 num = (int(input('Digite o 1˚ número: ')), int(input('Digite o 2˚ número: ')),
-       int(input('Digite o 3˚ número: ')), int(input('Digite o 4˚número: ')))
+       int(input('Digite o 3˚ número: ')), int(input('Digite o 4˚ número: ')))
 
 print(f'Você digitou os números: {num}')
 print(f'- O número 9 apareceu {num.count(9)} vezes.')
 if 3 in num:
     print(f'- O primeiro número 3 apareceu na posição {num.index(3)+1}.')
 else:
-    print('- O valor 3 não foi digitado')
-print('- Os valores pares digitados foram: ',end='')
-for n in num:
-    if n % 2 == 0:
-        print(n, end=' ')
-        
+    print('- O valor 3 não foi digitado.')
+
+# Criando lista com os números pares
+pares = [str(n) for n in num if n % 2 == 0]
+
+# Verificando se existem números pares
+if pares:
+    print(f'- Os valores pares digitados foram: {", ".join(pares)}')
+else:
+    print('- NENHUM NÚMERO PAR')
