c0ae3d1562ca8382d92d5a4cd4157432d267daee
Lais Correa <lais.crr@gmail.com>
2025-03-29 18:44:34 +0000

    Update ex073 - Tuplas com times de futebol.py

--- ex073 - Tuplas com times de futebol.py
+++ ex073 - Tuplas com times de futebol.py
@@ -21,6 +21,3 @@ if 'Chapecoense' in cla:
     print(f'- O Chapecoense está na {cla.index("Chapecoense")+1}˚posiçãp')
 else:
     print('- Infelizmente, o time Chapecoense não se classificou no Brasileirão.')
-
-
-
