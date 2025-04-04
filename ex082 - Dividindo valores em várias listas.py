b6bba0abeef3cfdbf687f9bdb66b0a2404eab927
Lais Correa <lais.crr@gmail.com>
2025-04-04 12:35:12 +0100

    Update ex082 - Dividindo valores em várias listas.py

--- ex082 - Dividindo valores em várias listas.py
+++ ex082 - Dividindo valores em várias listas.py
@@ -9,3 +9,13 @@ while True:
     r = str(input('Quer continuar? [S/N] ')).upper()
     if r in 'N':
         break
+        for valor in num:
+    if valor % 2 == 0:
+        pares.append(valor)
+    else:
+        impares.append(valor)
+print('-=' * 20)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
+                                                            
