32e292f8ab3f5fd5987342807d469e2f00758cf5
Lais Correa <lais.crr@gmail.com>
2025-04-04 12:34:49 +0100

    Create ex082 - Dividindo valores em várias listas.py

---
+++ ex082 - Dividindo valores em várias listas.py
@@ -0,0 +1,20 @@
+'''Crie um programa que vai ler vários números e colocar em uma lista.
+Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.'''
+
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um valor: ')))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+    if r in 'N':
+        break
+
+
+
+
+
+
+
+
+
