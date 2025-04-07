a9de1784bf6a645ef94961565fdb048b3ab2e91d
Lais Correa <lais.crr@gmail.com>
2025-04-07 11:31:05 +0100

    Update ex083 - Validando expressões matemáticas.py

--- ex083 - Validando expressões matemáticas.py
+++ ex083 - Validando expressões matemáticas.py
@@ -7,7 +7,7 @@ lista = []
 for simb in expressao:
     if simb == '(':
         lista.append('(')
-        
+
     elif simb == ')':
         if len(lista) > 0:
             lista.pop()
@@ -19,3 +19,4 @@ if len(lista) == 0:
     print('Sua expressão é válida!')
 else:
     print('Sua expressão é inválida')
+    
