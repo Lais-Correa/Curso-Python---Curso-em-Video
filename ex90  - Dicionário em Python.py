bdae477bc337e3cb920c07d5dec8354f2b3c7c80
Lais Correa <lais.crr@gmail.com>
2025-08-20 11:39:48 +0100

    Create ex90  - Dicionário em Python.py

---
+++ ex90  - Dicionário em Python.py
@@ -0,0 +1,16 @@
+#Faça um programa que leia nome e média de um aluno,
+# guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+
+aluno = {}
+
+aluno['Nome'] = str(input('Nome: '))
+aluno['Media'] = float(input(f'Média de {aluno['Nome']}: '))
+if aluno['Media'] >= 7.0:
+    aluno['Situação'] = 'Aprovado'
+elif 5<= aluno['Media'] < 7:
+    aluno['Situação'] = 'Recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+print('-='*30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
