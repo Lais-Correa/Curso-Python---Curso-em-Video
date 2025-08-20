a814a159f37fe41b9c8d0a686fe453cbdd205878
Lais Correa <lais.crr@gmail.com>
2025-08-20 11:43:12 +0100

    Update ex091 - Jogo de dados em Python.py

--- ex091 - Jogo de dados em Python.py
+++ ex091 - Jogo de dados em Python.py
@@ -2,6 +2,7 @@
 # Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem,
 # sabendo que o vencedor tirou o maior número no dado.
 
+
 from random import randint
 from time import sleep
 from operator import itemgetter
