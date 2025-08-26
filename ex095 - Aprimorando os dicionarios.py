d96f79dd1b5c2ebba7e7b5e04037a329f8dc9cfc
Lais Correa <lais.crr@gmail.com>
2025-08-26 12:37:24 +0100

    Create ex095 - Aprimorando os dicionarios.py

---
+++ ex095 - Aprimorando os dicionarios.py
@@ -0,0 +1,48 @@
+#Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
+jogador = {}
+time = []
+partidas = []
+while True:
+    jogador.clear()
+    jogador['Nome'] = str(input('Nome do Jogador: '))
+    total = int(input(f'Quantas partidas {jogador['Nome']} jogou? '))
+    partidas.clear()
+    for c in range(0,total):
+        partidas.append(int(input(f'Quantos gols na {c+1}˚ partida? ' )))
+    jogador['Gols'] = partidas[:]
+    jogador['Total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resposta = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO!, digite apenas S ou N.')
+    if resposta == 'N':
+        break
+print('-='*30)
+print(' Cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*40)
+for k, v in enumerate(time):
+    print(f'{k:>4} ',end='')
+    for d in v.values():
+        print(f'{str(d):<15}',end='')
+    print()
+print('-'*40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? [999 para parar] '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com código {busca}!')
+    else:
+        print(f'  -- LEVANTAMENTO DO JOGADOR {time[busca]['Nome']}: ')
+        for i, g in enumerate(time[busca]['Gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-'*40)
+print('<< VOLTE SEMPRE >>')
+
+
