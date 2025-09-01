8f7c99ea27bc76cff8b2a77b914e623e4fbbaeec
Lais Correa <lais.crr@gmail.com>
2025-09-01 11:38:26 +0100

    Create ex096 - Função que calcula área.py

---
+++ ex096 - Função que calcula área.py
@@ -0,0 +1,10 @@
+
+def area(* valores):
+    l=float(input('LARGURA (m): '))
+    c=float(input('COMPRIMENTO (m): '))
+    a = l*c
+    print(f'A área de um terreno {l} X {c} é de {a}m2')
+
+print(' Controle de terrenos ')
+print('-'*22)
+area()
