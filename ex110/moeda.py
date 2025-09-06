6fdd6501c36dc1fd09f16cb59b29765ce43e161f
Lais Correa <lais.crr@gmail.com>
2025-09-06 18:02:56 +0100

    ex110

--- ex110/moeda.py
+++ ex110/moeda.py
@@ -17,5 +17,18 @@ def metade(preço = 0, formato = False):
     return res if formato is False else moeda(res)
 
 
-def moeda(preço = 0, moeda = 'R$'):
+def moeda(preço = 0, moeda = 'R$ '):
     return f'{moeda}{preço:.2f}'.replace('.',',')
+
+
+def resumo(preço = 0, taxaa = 10, taxar = 5):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'Preço analisado: \t{moeda(preço)}')
+    print('-' * 30)
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço:\t{metade(preço, True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preço,taxaa,True)}')
+    print(f'{taxar}% de redução: \t\t{diminuir(preço,taxar,True)}')
+    print('-' * 30)
