02fd010be9fba2f7eb366d8e96adf49874cbac8e
Lais Correa <lais.crr@gmail.com>
2025-09-06 16:14:21 +0100

    ex107

--- ex107/teste.py
+++ ex107/teste.py
@@ -1,7 +1,7 @@
-import moeda
+from ex107 import moeda
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de R$ {p:.2f} é R$ {moeda.metade(p):.2f}')
-print(f'O dobro de R$ {p:.2f} é R$ {moeda.dobro(p):.2f}')
-print(f'Aumentando 10% temos R$ {moeda.aumentar(p,10):.2f}')
-print(f'Diminuindo 10% temos R$ {moeda.diminuir(p,10):.2f}')
+print(f'A metade de R$ {p} é R$ {moeda.metade(p)}')
+print(f'O dobro de R$ {p} é R$ {moeda.dobro(p)}')
+print(f'Aumentando 10% temos R$ {moeda.aumentar(p,10)}')
+print(f'Diminuindo 10% temos R$ {moeda.diminuir(p,10):}')
