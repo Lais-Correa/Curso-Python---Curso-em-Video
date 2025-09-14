2d562fb127b08ccd8bba46d064a49812ab110a64
Lais Correa <lais.crr@gmail.com>
2025-09-14 13:39:32 +0100

    ex115

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -10,13 +10,17 @@ if not arquivoExiste(arquivo):
 while True:
     resposta = menu(['VER PESSOAS CADASTRADAS', 'CADASTRAR NOVA PESSOA', 'SAIR DO SISTEMA'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        #opção de listar o conteudo do arquivo
+        lerArquivo(arquivo)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = LeiaInt('Idade: ')
+        cadastrar(arquivo, nome, idade)
     elif resposta == 3:
         cabeçalho('SAINDO DO SISTEMA...Até logo!')
         break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m')
-    sleep(1)
+    sleep(2)
 
